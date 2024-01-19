c71d9ff9fd5d1fd5eb46a5f4fd59769841657bdb
Juan Plasencia <jplasencia.dev@outlook.com>
2024-01-19 00:52:37 -0500

    Abstraction Learning Activity

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Job
+{
+    public String _company;
+    public String _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display(){
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} - {_endYear}");
+    }
+}
