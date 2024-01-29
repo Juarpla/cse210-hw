b42c2eb4307b595ab43593dd7575aa6ef25cc415
Juan Plasencia <jplasencia.dev@outlook.com>
2024-01-29 15:59:48 -0500

    Encapsulation Learning Activity completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fractionFirst = new Fraction();
+        Fraction fractionSecond = new Fraction(6);
+        Fraction fractionThird = new Fraction(6, 7);
+
+        int top = fractionFirst.GetTop();
+        Console.WriteLine(top);
+
+        fractionSecond.SetTop(5);
+        int secondTop = fractionSecond.GetTop();
+        Console.WriteLine(secondTop);
+
+        int ThirdBottom = fractionThird.GetBottom();
+        Console.WriteLine(ThirdBottom);
+
+        fractionSecond.SetBottom(10);
+        int secondBottom = fractionSecond.GetBottom();
+        Console.WriteLine(secondBottom);
+
+        Fraction fractionFourth = new Fraction();
+        Fraction fractionFiveth = new Fraction(10, 3);
+        Fraction fractionSixth = new Fraction(3);
+
+        Console.WriteLine("====================");
+
+        Console.WriteLine(fractionFirst.GetFractionString());
+        Console.WriteLine(fractionSecond.GetDecimalValue());
+        Console.WriteLine(fractionThird.GetFractionString());
+        Console.WriteLine(fractionFourth.GetFractionString());
+        Console.WriteLine(fractionFiveth.GetDecimalValue());
+        Console.WriteLine(fractionSixth.GetFractionString());
     }
 }
