d24095708a79b3e5525f885be54d1847e3aed103
Juan Plasencia <jplasencia.dev@outlook.com>
2024-01-29 16:09:27 -0500

    format fixed sintax

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,42 +5,51 @@ class Fraction
     private int _top;
     private int _bottom;
 
-    public Fraction(){
+    public Fraction()
+    {
         _top = 1;
         _bottom = 1;
     }
 
-    public Fraction(int wholeNumber){
+    public Fraction(int wholeNumber)
+    {
         _top = wholeNumber;
         _bottom = 1;
     }
 
-    public Fraction(int top, int bottom){
+    public Fraction(int top, int bottom)
+    {
         _top = top;
         _bottom = bottom;
     }
 
-    public int GetTop(){
+    public int GetTop()
+    {
         return _top;
     }
 
-    public void SetTop(int top){
+    public void SetTop(int top)
+    {
         _top = top;
     }
 
-    public int GetBottom(){
+    public int GetBottom()
+    {
         return _bottom; 
     }
 
-    public void SetBottom(int bottom){
+    public void SetBottom(int bottom)
+    {
         _bottom = bottom;
     }
 
-    public string GetFractionString(){
+    public string GetFractionString()
+    {
         return $"{_top}/{_bottom}";
     }
 
-    public double GetDecimalValue(){
+    public double GetDecimalValue()
+    {
         return (double) _top / _bottom;
     }
 }
